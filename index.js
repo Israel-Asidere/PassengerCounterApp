daf637f8172cf4f3b2087ce47dbbd0cdbbc00b84
Israel-Asidere <52455381+Israel-Asidere@users.noreply.github.com>
2022-04-14 18:47:32 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,47 @@
+
+
+// let count = 0
+// console.log(count)
+
+// let myAge = 21
+// let humanDogRatio = 7
+
+// let myDogAge = myAge* humanDogRatio
+// console.log(myDogAge)
+
+
+//  let bonusPoints =50
+//  bonusPoints = 25
+//  bonusPoints = 70
+//  console.log(bonusPoints)
+
+// let lapsComleted = 0
+// function totalLaps(){
+//     lapsComleted = lapsComleted + 1 
+// }
+
+// totalLaps()
+// totalLaps()
+// totalLaps()
+
+
+let countEl = document.getElementById('count-el')
+let count = 0
+
+function increment(){
+
+    count = count + 1
+    countEl.innerText = count
+    
+} 
+
+function save(){
+    console.log(count)
+}
+let username = "Pierre"
+
+let message = "You have three new notifications!"
+
+let messageToUser = username + ", " + message
+
+console.log(messageToUserb )
